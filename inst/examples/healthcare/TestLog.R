1570ad9a13932fc09c7eb900552f7964b2a415a3
Gongol <b.gongol@ruhealth.org>
2026-07-13 14:58:44 -0700

    Optimizing database

---
+++ inst/examples/healthcare/TestLog.R
@@ -0,0 +1,9 @@
+
+Error in .apply_compatibility_review(registry, compatibility, strict = strict) : 
+  109 compatibility decision(s) remain unresolved in CompatibilityDecisions. First unresolved: cross_database/ALL/AGE, cross_database/ALL/AGE_NEONATE, cross_database/ALL/AIDS, cross_database/ALL/AMONTH, cross_database/ALL/AWEEKEND
+In addition: 
+  Warning messages:
+  1: In `[.data.table`(compatibility, hit, `:=`((field), decisions[[field]][idx[hit]])) :
+  Coercing 'character' RHS to 'double' to match the type of column 12 named 'Decision'.
+2: In `[.data.table`(compatibility, hit, `:=`((field), decisions[[field]][idx[hit]])) :
+  NAs introduced by coercion
